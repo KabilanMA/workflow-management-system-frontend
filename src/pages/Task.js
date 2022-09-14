1ff264a5ea90bdbb97240a1d9850a68b8af411de
Pasan SK <pasanxavioz@gmail.com>
2022-09-14 09:49:18 +0530

    Impplemented backend part to fetch mainTasks data in tasks page

--- src/pages/Task.js
+++ src/pages/Task.js
@@ -1,19 +1,38 @@
 import { Link as RouterLink } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 // material
 import { Grid, Button, Container, Stack, Typography } from '@mui/material';
 // components
 
 import Page from '../components/Page';
 import Iconify from '../components/Iconify';
+import useAxiosPrivate from '../hooks/useAxiosPrivate'
 
 import { TaskCard, TaskForm } from '../sections/@dashboard/task';
 // mock
-import POSTS from '../_mock/blog';
+// import POSTS from '../_mock/blog';
 
 // ----------------------------------------------------------------------
 
+const ALL_MAINTASKS_URL = "/mainTasks"
+
 export default function TaskPage() {
+
+  const [mainTasks, setMainTasks] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const axios = useAxiosPrivate();
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(ALL_MAINTASKS_URL, {
+        withCredentials: true
+      });
+      setMainTasks(response?.data)
+      setIsLoading(false)
+    }
+    fetchData()
+  }, [])
+
   const [newTaskVisible, setNewTask] = useState(false);
     return(
         <Page title="Tasks">
@@ -52,8 +71,8 @@ export default function TaskPage() {
           </Stack> */}
   
           <Grid container spacing={3}>
-            {POSTS.map((post, index) => (
-              <TaskCard key={post.id} task={post} index={index} />
+            {!isLoading && mainTasks.map((maintask, index) => ( 
+              <TaskCard key={maintask._id} task={maintask} index={index} />
             ))}
           </Grid>
         </Container>
