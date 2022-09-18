830b5a5b20627a18f595c20be2b1036368238095
Pasan SK <pasanxavioz@gmail.com>
2022-09-18 09:48:43 +0530

    Implemented the backend part to save maintask and all subtasks

--- src/sections/@dashboard/task/new/TaskForm.js
+++ src/sections/@dashboard/task/new/TaskForm.js
@@ -10,12 +10,15 @@ import { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 // import axios from '../../../../api/axios';
 import useAxiosPrivate from '../../../../hooks/useAxiosPrivate';
+import { errorToast, successToast } from '../../../../components/Toasts';
 
 import CurrencyInputField from '../../../../components/CurrencyInputField';
 import SelectTextField from '../../../../components/SelectTextField';
 import SubTask from './SubTask';
 
 const ALL_CATEGORIES_URL = "/categories"
+const MAINTASK_URL = "/mainTasks"
+const SUBTASKS_URL = "/subtasks"
 
 // const tasks = [
 //   {
@@ -42,14 +45,93 @@ export default function TaskForm() {
   const location = useLocation()
   const [categories, setcategories] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
+  const [arr, setArr] = useState([]);
 
   const methods = useForm({ defaultValues });
   const { handleSubmit, reset, control, setValue, watch } = methods;
-  const onSubmit = (data) => {
-    console.log(data);
 
-    // If data successfully uploaded to database then refresh the page.
-    // window.location.reload(false);
+  const onSubmit = async (data) => {
+
+    let mainTaskObj = {}
+
+    Object.keys(data).forEach(key => {
+      if (key === "taskCategory") {
+        const pair = { "category_id": data[`${key}`] }
+        mainTaskObj = { ...mainTaskObj, ...pair }
+      } else if (key === "description") {
+        const pair = { "description": data[`${key}`] }
+        mainTaskObj = { ...mainTaskObj, ...pair }
+      } else if (key === "totalBudget") {
+        const pair = { "budget": data[`${key}`] }
+        mainTaskObj = { ...mainTaskObj, ...pair }
+      }
+    });
+
+    let allSubTasks = []
+
+    for (let i = 0; i < arr.length; i += 1) {
+      let oneSubTask = {}
+      oneSubTask = { ...oneSubTask, ...{ "name": data[`taskName ${i}`] } }
+      oneSubTask = { ...oneSubTask, ...{ "note": data[`taskNote ${i}`] } }
+      oneSubTask = { ...oneSubTask, ...{ "deadline": data[`deadline ${i}`] } }
+
+      let assignedEmployees = {}
+
+      for (let j = 0; j < 100; j += 1) { // 100 is hardcoded ??
+        if (data[`employee ${i} ${j}`]) {
+          const employeeData = data[`employee ${i} ${j}`]
+          const empID = employeeData._id
+          const pair = {}
+          pair[`${empID}`] = false
+          assignedEmployees = { ...assignedEmployees, ...pair }
+        }
+      }
+      oneSubTask = { ...oneSubTask, ...{ "assigned_employees": assignedEmployees } }
+      allSubTasks = [...allSubTasks, oneSubTask]
+    }
+
+    let isError = false
+    
+    try {
+      const response1 = await axios.post(MAINTASK_URL, {
+        ...mainTaskObj,
+      })
+      const newMainTaskID = response1.data._id
+      console.log("Maintask saved in db: ")
+      console.log(newMainTaskID)
+
+      allSubTasks.forEach(async subtask => {
+        console.log("subtask to be entered: ")
+        console.log(subtask)
+        subtask = { ...subtask, "maintask_id": newMainTaskID }
+        try {
+          const response2 = await axios.post(SUBTASKS_URL, {
+            ...subtask
+          })
+          console.log('subtask res: ', response2)
+        } catch (err) {
+          isError = true
+          console.error("ERROR WHEN SUBMITTING A SUBTASK")
+          console.log(err)
+          if (err.response.status === 400) { // Bad request
+            errorToast("Error: Please complete all the fields before submitting")
+          }
+        }
+      })
+    } catch (err) {
+      isError = true
+      console.error("ERROR WHEN SUBMITTING THE MAINTASK")
+      console.log(err)
+      if (err.response.status === 400) { // Bad request
+        errorToast("Error: Please complete all the fields before submitting")
+      }
+    }
+    if (!isError) {
+      console.log("IIIIIIII", isError)
+      // window.location.reload(false);
+      navigate("/dashboard/task")
+      // successToast("Successfully created the workflow!")
+    }
   }
 
   useEffect(() => {
@@ -62,24 +144,26 @@ export default function TaskForm() {
           signal: controller.signal,
           withCredentials: true
         });
-        if(isMounted) setcategories(response?.data)
-        if(isMounted) setIsLoading(false)
+        if (isMounted) setcategories(response?.data)
+        if (isMounted) setIsLoading(false)
       } catch (err) {
         console.error("ERROR IN USEEFFECT : ")
         console.log(err)
-        navigate('/login', { state: { from: location }, replace: true })
+        if (err.response.status === 204) { // No content
+          errorToast("No categories yet")
+        } else {
+          navigate('/login', { state: { from: location }, replace: true })
+        }
       }
     }
 
     fetchData()
 
     return () => {
-      isMounted = false 
+      isMounted = false
       controller.abort()
     }
   }, [])
-
-  const [arr, setArr] = useState([]);
 
   const createNewTaskField = () => {
     setArr(data => {
