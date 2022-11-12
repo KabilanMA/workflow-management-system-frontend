3f1e89ce9c349ba1b400448ebe4ea56ac6911f58
Pasan SK <pasanxavioz@gmail.com>
2022-11-12 10:32:48 +0530

    Implemented search by name facility for MainTasks-list page

--- src/pages/Task.js
+++ src/pages/Task.js
@@ -1,5 +1,6 @@
 import { Link as RouterLink, useNavigate, useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { filter } from 'lodash';
 // material
 import { Grid, Button, Container, Stack, Typography } from '@mui/material';
 // components
@@ -8,6 +9,7 @@ import Iconify from '../components/Iconify';
 import useAxiosPrivate from '../hooks/useAxiosPrivate'
 
 import { TaskCard } from '../sections/@dashboard/task';
+import TaskListToolbar from '../components/TaskListToolbar';
 
 const ALL_MAINTASKS_URL = "/mainTasks"
 
@@ -15,9 +17,14 @@ export default function TaskPage() {
 
   const [mainTasks, setMainTasks] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
+  const [filterName, setFilterName] = useState('');
   const axios = useAxiosPrivate();
   const navigate = useNavigate()
   const location = useLocation()
+
+  const handleFilterByName = (event) => {
+    setFilterName(event.target.value);
+  };
 
   useEffect(() => {
     let isMounted = true
@@ -41,12 +48,49 @@ export default function TaskPage() {
     fetchData()
 
     return () => {
-      isMounted = false 
+      isMounted = false
       controller.abort()
     }
   }, [])
 
   const [newTaskVisible, setNewTask] = useState(false);
+  const [order, setOrder] = useState('asc');
+  const [orderBy, setOrderBy] = useState('name');
+
+  function applySortFilter(array, comparator, query) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    if (query) {
+      return filter(array, (_maintask) => _maintask.description.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+    }
+    return stabilizedThis.map((el) => el[0]);
+  }
+
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+
+  function getComparator(order, orderBy) {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+
+  let filteredTasks = []
+  if (!isLoading) {
+    filteredTasks = applySortFilter(mainTasks, getComparator(order, orderBy), filterName);
+  }
+
   return (
     <Page title="Tasks">
       <Container>
@@ -69,7 +113,8 @@ export default function TaskPage() {
         </Stack>
 
         <Grid container spacing={3}>
-          {!isLoading && mainTasks.map((maintask, index) => (
+          <TaskListToolbar filterName={filterName} onFilterName={handleFilterByName} />
+          {!isLoading && filteredTasks.map((maintask, index) => (
             <TaskCard key={maintask._id} task={maintask} index={index} />
           ))}
         </Grid>
